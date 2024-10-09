dad850047d013bbcce07ca80d1ea4eb5725c670d
npfister.6822@gmail.com <npfister.6822@gmail.com>
2024-10-09 10:32:49 +0000

    Created Database_Server; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +12,11 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+    self.drop_down_1.items = [("Satteins", 0), ("Mordor", 1)]
+    print(anvil.server.call("say_hello", "sauron"))
+    print(anvil.server.call("get_jugendherbergen"))
+
+    self.drop_down_1.items = anvil.server.call('get_jugendherbergen', "name, JID")
+
+    
---
+++ server_code/Database_Server.py
@@ -0,0 +1,27 @@
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.files
+from anvil.files import data_files
+import anvil.server
+import sqlite3
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+@anvil.server.callable
+def say_hello(name):
+  print("Hello, " + name + "!")
+  return 42
+
+@anvil.server.callable
+def get_jugendherbergen(rows="*"):
+  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung 1.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows} FROM jugendherbergen"))
+  print(res)
+  return res
