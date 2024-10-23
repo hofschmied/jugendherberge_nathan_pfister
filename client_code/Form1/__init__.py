78b70b1981fe4f0bfbeb877387abe8723f68fdd5
npfister.6822@gmail.com <npfister.6822@gmail.com>
2024-10-23 09:43:02 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -12,14 +12,15 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-    self.drop_down_1.items = [("Satteins", 0), ("Mordor", 1)]
+    self.drop_down_2.items = [("Satteins", 0), ("Mordor", 1)]
     print(anvil.server.call("say_hello", "sauron"))
-    print(anvil.server.call("get_jugendherbergen"))
-
+    print(anvil.server.call("get_jugendherbergen", self.drop_down_1.items("JID")))
     self.drop_down_1.items = anvil.server.call('get_jugendherbergen', "name, JID")
 
+def drop_down_2_change(self, **event_args):
+  self.drop_down_2.items[self.drop_down_1.selected_value -1][1]
+
 def drop_down_1_change(self, **event_args):
-  self.drop_down_1.items[self.drop_down_1.selected_value -1][1]
+  self.drop_down_1.items["Zwiebelring", "Fidibus", "Wasseramöbe"]
 
     
