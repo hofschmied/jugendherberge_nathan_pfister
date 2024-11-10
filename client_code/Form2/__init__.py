829fed110587b9111e8e7ff0d6ffc22289a0125a
npfister.6822@gmail.com <npfister.6822@gmail.com>
2024-11-10 12:32:06 +0000

    Edited Database_Server and Form2

--- client_code/Form2/__init__.py
+++ client_code/Form2/__init__.py
@@ -3,43 +3,31 @@ from anvil import *
 import anvil.server
 
 class Form2(Form2Template):
-    def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
-        self.init_components(**properties)
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
 
-        # Jugendherbergen-Dropdown befüllen
-        self.drop_down_1.items = anvil.server.call("get_jugendherbergen")
+    # Any code you write here will run before the form opens.
+    
+    print(anvil.server.call("get_jugendherbergen"))
+    jugendherbergen = anvil.server.call('get_jugendherbergen', "name, JID")
+    self.drop_down_1.items = jugendherbergen
 
-    def update_rooms_dropdown(self, jugendherberge_id):
-        """Aktualisiert die Zimmer-Dropdownliste für die ausgewählte Jugendherberge"""
-        # Holt nur die Zimmer für die ausgewählte Jugendherberge
-        rooms = anvil.server.call("get_rooms", jugendherberge_id)
-        # Befüllt drop_down_2 mit den gefilterten Zimmern
-        self.drop_down_2.items = [(f"Zimmer {zimmernummer} - {preis_pro_nacht}€", zimmernummer)
-                                  for zimmernummer, bettenanzahl, preis_pro_nacht in rooms]
+    print(anvil.server.call("get_preiskategorie_for_jugendherbergen"))
+    self.drop_down_2.items = anvil.server.call('get_preiskategorie_for_jugendherbergen', "name, PID")
 
-    def drop_down_1_change(self, **event_args):
-        """Diese Methode wird aufgerufen, wenn eine Jugendherberge ausgewählt wird"""
-        selected_jugendherberge_id = self.drop_down_1.selected_value
-        if selected_jugendherberge_id is not None:
-            # Aktualisiert die Zimmer-Dropdownliste für die ausgewählte Jugendherberge
-            self.update_rooms_dropdown(selected_jugendherberge_id)
+  
+    jid_to_name = {jid: name for name, jid in jugendherbergen}
 
-    def button_1_click(self, **event_args):
-        """Diese Methode wird aufgerufen, wenn der Buchen-Button geklickt wird"""
-        pass
+    print(anvil.server.call("get_zimmer_for_jugendherbergen"))
+    self.listezimmer = [(zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID) for zimmernummer, bettenanzahl, preis_pro_nacht, gebucht, JID, ZID in anvil.server.call('get_zimmer_for_jugendherbergen')]
 
-    def date_picker_1_change(self, **event_args):
-        """Diese Methode wird aufgerufen, wenn das Datum im ersten DatePicker geändert wird"""
-        # Hole das ausgewählte Datum im ersten DatePicker
-        selected_date_1 = self.date_picker_1.date
-
-        # Überprüfe, ob das ausgewählte Datum im ersten DatePicker nicht None ist
-        if selected_date_1 is not None:
-            # Setze das min_date im zweiten DatePicker auf das ausgewählte Datum aus dem ersten DatePicker
-            self.date_picker_2.min_date = selected_date_1
-            self.date_picker_2.selected_date = None  # Setze das ausgewählte Datum im zweiten DatePicker zurück
-
-    def date_picker_2_change(self, **event_args):
-        """Diese Methode wird aufgerufen, wenn das Datum im zweiten DatePicker geändert wird"""
-        pass
+    liste = []
+    
+    for i in self.listezimmer:
+     jugendherberge_name = jid_to_name.get(i[5], "Unknown")
+     status = "Verfügbar" if not i[4] else "Gebucht"
+     liste.append(f"Zimmernummer: {i[1]}, Bettenanzahl: {i[2]}, Preis: {i[3]}€, Jugendherberge: {jugendherberge_name}, Verfügbarkeitsstatus: {status}")
+      
+    
+    self.drop_down_3.items = liste
--- server_code/Database_Server.py
+++ server_code/Database_Server.py
@@ -1,29 +1,40 @@
-import sqlite3
-import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.files
 from anvil.files import data_files
+import anvil.server
+import sqlite3
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+
 
 @anvil.server.callable
-def say_hello(name):
-    print("Hello, " + name + "!")
-    return 42
+def get_jugendherbergen(rows="*"):
+  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows} FROM jugendherbergen"))
+  print(res)
+  return res
 
 @anvil.server.callable
-def get_jugendherbergen():
-    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
-    cursor = conn.cursor()
-    res = list(cursor.execute("SELECT name, JID FROM jugendherbergen"))
-    conn.close()
-    return res
+def get_preiskategorie_for_jugendherbergen(rows="*"):
+  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows} FROM preiskategorie"))
+  print(res)
+  return res
 
 @anvil.server.callable
-def get_rooms(jugendherberge_id):
-    conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
-    cursor = conn.cursor()
-    # Abfrage der Zimmer nur für die ausgewählte Jugendherberge
-    res = list(cursor.execute("""
-        SELECT zimmernummer, bettenanzahl, preis_pro_nacht 
-        FROM zimmer 
-        WHERE JID = ? AND gebucht = 0
-    """, (jugendherberge_id,)))
-    conn.close()
-    return res
+def get_zimmer_for_jugendherbergen(rows="*"):
+  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows} FROM zimmer"))
+  print(res)
+  return res
